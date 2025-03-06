216466a66daeaa10f2e318d4d8640306e2676e72
hojin535 <123153695+hojin535@users.noreply.github.com>
2025-03-06 20:53:17 +0900

    first commit

---
+++ app/(movies)/movies/[id]/error.tsx
@@ -0,0 +1,4 @@
+"use client";
+export default function ErrorOMG() {
+  return <h1>lol someting broke...</h1>;
+}
---
+++ app/not-found.tsx
@@ -0,0 +1,13 @@
+import { Metadata } from "next";
+import Navigation from "../components/navigation";
+
+export const metadata: Metadata = {
+  title: "Not found",
+};
+export default function NotFound() {
+  return (
+    <div>
+      <h1> NotFound</h1>;
+    </div>
+  );
+}
