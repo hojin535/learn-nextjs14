c3fdd4b391320821b447ab822af8741508cd0862
hojin535 <123153695+hojin535@users.noreply.github.com>
2025-03-09 12:08:39 +0900

    feat: next.js15 이후 변경된 코드드

--- app/movies/[id]/page.tsx
+++ app/movies/[id]/page.tsx
@@ -1,25 +1,23 @@
 import MovieVideos from "../../../components/movie-videos";
 import MovieInfo, { getMovie } from "../../../components/movie-info";
-import { Suspense } from "react";
+import { JSX, Suspense } from "react";
 import MovieCredit from "../../../components/movie/credit/movie-credit";
 
-interface IParams {
-  params: { id: string };
-}
-export async function generateMetadata({ params }: IParams) {
-  const { id } = await params;
+type IParams = Promise<{
+  id: string;
+}>;
+export async function generateMetadata(props: { params: IParams }) {
+  const param = await props.params;
+  const id = param.id;
   // `id`를 사용하기 전에 비동기적으로 `params`를 가져옵니다.
   const movie = await getMovie(id); // `getMovie` 함수에서 비동기적으로 데이터를 가져오기
   return {
     title: `${movie.title}`,
   };
 }
-export default async function MovieDetail({
-  params,
-}: {
-  params: { id: string };
-}) {
-  const { id } = await params;
+export default async function MovieDetail(props: { params: IParams }) {
+  const param = await props.params;
+  const id = param.id;
   return (
     <div>
       <Suspense fallback={<h1>Loading movie info</h1>}>
